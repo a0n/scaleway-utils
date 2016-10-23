56edf6b6f3033ca5d01ad73f499d9b86c532f04c
Aaron Israel <aaron.israel@cedis.fu-berlin.de>
2016-10-24 01:50:45 +0200

    tinc

--- tinc/add-tinc.sh
+++ tinc/add-tinc.sh
@@ -17,30 +17,28 @@ MYTINCIP=$(ip addr show tun0 | grep -o 'inet [^/]*' | cut -d' ' -f 2)
 MYIP=$(ip addr show eth0 | grep -o 'inet [^/]*' | cut -d' ' -f 2)
 MYNAME=$(cat $TINCPATH/tinc.conf | awk '/Name/ { print $3}')
 
-ssh -l root $HOST "export PS4=\"\[\033[32;1m++++\[\033[0m \" ;\
-set -ex;\
-apt-get install -y tinc;\
-\
-mkdir -p ${TINCPATH}/hosts;\
-\
-if [ ! -f ${TINCPATH}/tinc.conf ]; then \
-	echo \"Name = ${TINCNAME}\" > ${TINCPATH}/tinc.conf;\
-	echo \"AddressFamily = ipv4\" >> ${TINCPATH}/tinc.conf;\
-	echo \"Interface = tun0\" >> ${TINCPATH}/tinc.conf;\
-	echo \"ConnectTo = $MYNAME\" >> ${TINCPATH}/tinc.conf;\
+ssh -l root $HOST "export PS4=\"\[\033[32;1m++++\[\033[0m \"; \
+set -ex; \
+apt-get install -y tinc; \
+mkdir -p ${TINCPATH}/hosts; \
+\if [ ! -f ${TINCPATH}/tinc.conf ]; then \
+	echo \"Name = ${TINCNAME}\" > ${TINCPATH}/tinc.conf; \
+	echo \"AddressFamily = ipv4\" >> ${TINCPATH}/tinc.conf; \
+	echo \"Interface = tun0\" >> ${TINCPATH}/tinc.conf; \
+	echo \"ConnectTo = $MYNAME\" >> ${TINCPATH}/tinc.conf; \
 fi \
 \
 if [ ! -f ${TINCPATH}/hosts/${TINCNAME} ]; then \
-	echo \"Subnet = ${TINCIP}/32\" > ${TINCPATH}/hosts/${TINCNAME};\
+	echo \"Subnet = ${TINCIP}/32\" > ${TINCPATH}/hosts/${TINCNAME}; \
 	tincd -n $VPNNAME -K4096 \
 fi \
 \
-echo '#!/bin/sh' > ${TINCPATH}/tinc-up;\
-echo 'ifconfig \$INTERFACE '$TINCIP' netmask 255.255.255.0' >> ${TINCPATH}/tinc-up;\
+echo '#!/bin/sh' > ${TINCPATH}/tinc-up; \
+echo 'ifconfig \$INTERFACE '$TINCIP' netmask 255.255.255.0' >> ${TINCPATH}/tinc-up; \
 \
-echo '#!/bin/sh' > ${TINCPATH}/tinc-down;\
-echo 'ifconfig \$INTERFACE down' >> ${TINCPATH}/tinc-down;\
-chmod +x ${TINCPATH}/tinc-{up,down};\
+echo '#!/bin/sh' > ${TINCPATH}/tinc-down; \
+echo 'ifconfig \$INTERFACE down' >> ${TINCPATH}/tinc-down; \
+chmod +x ${TINCPATH}/tinc-{up,down}; \
 \
 modprobe tun"
 
