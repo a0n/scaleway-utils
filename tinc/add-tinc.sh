b0d9e2446b6d0f20324cfab1870c83756b7114aa
Aaron Israel <aaron.israel@cedis.fu-berlin.de>
2016-10-24 01:30:20 +0200

    tinc

--- tinc/add-tinc.sh
+++ tinc/add-tinc.sh
@@ -19,45 +19,41 @@ MYNAME=$(cat $TINCPATH/tinc.conf | awk '/Name/ { print $3}')
 
 # rsync -Pavvzessh /var/cache/apt/archives/{tinc_*.deb,liblzo2-*.deb} root@${HOST}:/tmp
 
-ssh -l root $HOST <<"EOT"
-export PS4="\[\033[32;1m++++\[\033[0m "
-set -ex
-apt-get install -y tinc
+ssh -l root $HOST "
+export PS4=\"\[\033[32;1m++++\[\033[0m \" && \
+set -ex && \
+apt-get install -y tinc && \
+mkdir -p $TINCPATH/hosts && \
+if [ ! -f ${TINCPATH}/tinc.conf ]; then \
+	echo "Name = ${TINCNAME}" > ${TINCPATH}/tinc.conf ; \
+	echo "AddressFamily = ipv4" >> ${TINCPATH}/tinc.conf ; \
+	echo "Interface = tun0" >> ${TINCPATH}/tinc.conf ; \
+	echo "ConnectTo = $MYNAME" >> ${TINCPATH}/tinc.conf ; \
+fi"
 
-# dpkg -l tinc > /dev/null || dpkg -i /tmp/liblzo2-*.deb /tmp/tinc_*.deb
+# if [ ! -f ${TINCPATH}/hosts/${TINCNAME} ]; then
+# 	echo "Subnet = ${TINCIP}/32" > ${TINCPATH}/hosts/${TINCNAME}
+# 	tincd -n $VPNNAME -K4096
+# fi
 
-mkdir -p $TINCPATH/hosts
+# echo "#!/bin/sh" > ${TINCPATH}/tinc-up
+# echo 'ifconfig \$INTERFACE '$TINCIP' netmask 255.255.255.0' >> ${TINCPATH}/tinc-up
 
-if [ ! -f ${TINCPATH}/tinc.conf ]; then
-	echo "Name = ${TINCNAME}" > ${TINCPATH}/tinc.conf
-	echo "AddressFamily = ipv4" >> ${TINCPATH}/tinc.conf
-	echo "Interface = tun0" >> ${TINCPATH}/tinc.conf
-	echo "ConnectTo = $MYNAME" >> ${TINCPATH}/tinc.conf
-fi
+# echo "#!/bin/sh" > ${TINCPATH}/tinc-down
+# echo 'ifconfig \$INTERFACE down' >> ${TINCPATH}/tinc-down
+# chmod +x ${TINCPATH}/tinc-{up,down}
 
-if [ ! -f ${TINCPATH}/hosts/${TINCNAME} ]; then
-	echo "Subnet = ${TINCIP}/32" > ${TINCPATH}/hosts/${TINCNAME}
-	tincd -n $VPNNAME -K4096
-fi
+# modprobe tun
+# EOT
 
-echo "#!/bin/sh" > ${TINCPATH}/tinc-up
-echo 'ifconfig \$INTERFACE '$TINCIP' netmask 255.255.255.0' >> ${TINCPATH}/tinc-up
+# rsync -Pavvzessh ${TINCPATH}/hosts/$MYNAME $HOST:${TINCPATH}/hosts/
+# rsync -Pavvzessh $HOST:${TINCPATH}/hosts/${TINCNAME} ${TINCPATH}/hosts/
+# tincd -n $VPNNAME -k HUP || tincd -n $VPNNAME
 
-echo "#!/bin/sh" > ${TINCPATH}/tinc-down
-echo 'ifconfig \$INTERFACE down' >> ${TINCPATH}/tinc-down
-chmod +x ${TINCPATH}/tinc-{up,down}
-
-modprobe tun
-EOT
-
-rsync -Pavvzessh ${TINCPATH}/hosts/$MYNAME $HOST:${TINCPATH}/hosts/
-rsync -Pavvzessh $HOST:${TINCPATH}/hosts/${TINCNAME} ${TINCPATH}/hosts/
-tincd -n $VPNNAME -k HUP || tincd -n $VPNNAME
-
-ssh -l root $HOST <<'EOT'
-tincd -n $VPNNAME -k HUP || tincd -n $VPNNAME
-GATEWAY=\$(route -n | grep '^0.0.0.0 .*eth0' | awk '{print \$2}')
-route add -host $MYIP gw \$GATEWAY
-route add default gw $MYTINCIP
-route del default gw \$GATEWAY
-EOT
+# ssh -l root $HOST <<'EOT'
+# tincd -n $VPNNAME -k HUP || tincd -n $VPNNAME
+# GATEWAY=\$(route -n | grep '^0.0.0.0 .*eth0' | awk '{print \$2}')
+# route add -host $MYIP gw \$GATEWAY
+# route add default gw $MYTINCIP
+# route del default gw \$GATEWAY
+# EOT
